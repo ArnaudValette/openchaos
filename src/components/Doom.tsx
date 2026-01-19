6c9b0beadeabe3a9ecb65a31fcb7dca23b99f435
arnaudvalette <contact@valettearnaud.dev>
2026-01-19 19:04:26 +0100

    fix » build errors

--- src/components/Doom.tsx
+++ src/components/Doom.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from "react"
+import { useState, MouseEventHandler } from "react"
 
 declare global {
   interface Window {
@@ -8,73 +8,21 @@ declare global {
   }
 }
 
-function RunDoom({ onClose }) {
+function RunDoom({ onClose }: { onClose: MouseEventHandler<HTMLDivElement> }) {
   return (
     <>
       <div className="freedoom-blur" onClick={onClose} />
       <div className="freedoom-computer">
         <div className="freedoom-title">FREEDOOM</div>
-      <div className="freedoom-dialog">
-        <iframe
-          src="/doom.html"
-          className="freedoom-iframe"
-          allow="autoplay"
-        />
-        <div className="freedoom-scanline"></div>
+        <div className="freedoom-dialog">
+          <iframe
+            src="/doom.html"
+            className="freedoom-iframe"
+            allow="autoplay"
+          />
+          <div className="freedoom-scanline"></div>
+        </div>
       </div>
-      </div>
-    </>
-  )
-}
-
-function RunDoom2({ setToggle }) {
-  function shutdown(){
-    window.location.reload()
-  }
-  useEffect(() => {
-    if (!window.Module) {
-      window.Module = {
-        locateFile: (path: string) => `${path}`,
-        canvas: document.getElementById("canvas"),
-        print: (text: string) => console.log(text),
-        printErr: (text: string) => console.error(text),
-      }
-
-    }
-      const script = document.createElement("script")
-      script.src = `/doomgeneric.js`
-      script.id = "freedoom-script"
-      script.async = true
-      document.body.appendChild(script)
-  }, [])
-
-  return (
-    <>
-      <div className="freedoom-blur" onClick={() => shutdown()}></div>
-      <div className="freedoom-dialog">
-        <canvas
-          className="freedoom-canvas"
-          id="canvas"
-          onContextMenu={(e) => e.preventDefault()}
-          tabIndex={-1}
-        ></canvas>
-        <div className="freedoom-scanline"></div>
-      </div>
-    </>
-  )
-}
-
-export function OldDoom() {
-  const [toggle, setToggle] = useState(false)
-  return (
-    <>
-      <button
-        onClick={() => setToggle(true)}
-        className="launch-freedoom-button"
-      >
-        FREEDOOM
-      </button>
-      {toggle && <RunDoom key={Date.now()} setToggle={setToggle} />}
     </>
   )
 }
@@ -84,7 +32,9 @@ export function Doom() {
 
   return (
     <>
-      <button className="launch-freedoom-button" onClick={() => setOpen(true)}>FREEDOOM</button>
+      <button className="launch-freedoom-button" onClick={() => setOpen(true)}>
+        FREEDOOM
+      </button>
       {open && <RunDoom onClose={() => setOpen(false)} />}
     </>
   )
